d937c9cc177e246a213c2a6d5e2e1a6cab8058db
Frode Aannevik <frode.aa@gmail.com>
2018-11-04 20:43:54 +0100

    remove the need for config (#4)
    update the project source type to `NO-SOURCE`
    and embed the BuildSpec directly into the
    template
    
    simplifies the template dependencies and
    usage
    
    fixes: https://github.com/frodeaa/renovate-aws/issues/2

--- config/renovate-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  onboardingConfig: { extends: ['config:base'] },
-  repositories: process.env.RENOVATE_REPOSITORIES ?
-    process.env.RENOVATE_REPOSITORIES.split(",").map(v => v.trim()) : []
-};
--- scripts/release.sh
+++ scripts/release.sh
@@ -13,20 +13,6 @@ release_cf_template() {
     aws s3 cp "${TEMPLATE_PATH}" "s3://${bucket}/cf-template/latest/renovate.yml"
 }
 
-release_config() {
-    CONFIG=$1
-    VERSION=$2
-
-    TMPDIR=$(mktemp -d)
-    zip -r --junk-paths "${TMPDIR}/config" "${CONFIG}"
-
-    aws s3 cp "${TMPDIR}/config.zip" "s3://${bucket}/config/${VERSION}/config.zip"
-    aws s3 cp "${TMPDIR}/config.zip" "s3://${bucket}/config/latest/config.zip"
-}
-
 TEMPLATE="${root}/cloudformation/renovate.yml"
-CONFIG="${root}/config"
 VERSION="$(git rev-parse --short HEAD)"
-
 release_cf_template "${TEMPLATE}" "${VERSION}"
-release_config "${CONFIG}" "${VERSION}"
